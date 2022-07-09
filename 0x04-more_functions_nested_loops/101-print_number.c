bd025da5078b0b79040ddee66d6f1ea9fbf20c3c
lweyajoe <joseph.lweya@gmail.com>
2022-07-09 14:56:37 +0300

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
+void print(unsigned int n);
+
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - print number
+ * @var: variable to be printed
+ *
+ * Created by: Sanctus-Peter
+ * cc: 7th july, 2022
+ *
  */
-void print_number(int n)
+void print_number(int var)
 {
-unsigned int m, d, count;
+	if (var < 0)
+	{
+		_putchar('-');
+		var = -var;
+	}
 
-if (n < 0)
-{
-_putchar(45);
-m = n * -1;
-}
-else
-{
-m = n;
+	print(var);
 }
 
-d = m;
-count = 1;
+/**
+ * print - subfunction to print recursively
+ * @n: variable to be printed
+ */
 
-while (d > 9)
+void print(unsigned int n)
 {
-d /= 10;
-count *= 10;
-}
-
-for (; count >= 1; count /= 10)
-{
-_putchar(((m / count) % 10) + 48);
-}
+	if (n / 10)
+		print(n / 10);
+	_putchar('0' + n % 10);
 }

80fb3aa3a25cc7e9cfb73378d8170b8f69b78a24
lweyajoe <joseph.lweya@gmail.com>
2022-09-30 07:39:56 +0300

    changes in 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print(int var)
  * print_times_table - prints the n times table, starting with 0
  * @n: time table to print
  *
- * Created by - Lweya Joe
+ * Created by - Sanctus-Peter
  * cc; 5th july, 2022
  */
 
